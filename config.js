3d6d0e0b24c32247ffdd4df5116bc0d3350f0302
Dirk <vanroosmalendirk@gmail.com>
2024-02-08 10:05:52 +0100

    Migrated all settings to Vigilance
    Migrated all settings to Vigilance, needs testing and debugging

--- config.js
+++ config.js
@@ -7,14 +7,57 @@ import { @Vigilant, @TextProperty, @ColorProperty, @ButtonProperty, @SwitchPrope
   },
 })
 class Settings {
-    @SelectorProperty({
-        name: "Vanquisher chat",
-        description: "Select a chat to send vanquisher messages to",
-        category: "Chat utilities",
-        subcategory: "Vanquishers",
-        options: ["OFF", "ALL", "PARTY", "GUILD", "COOP"]
-    })
-    vanqMode = 0;
+  @SelectorProperty({
+    name: "Vanquisher messages",
+    description: "Select a chat to send vanquisher messages to",
+    category: "Chat utilities",
+    subcategory: "Vanquishers",
+    options: ["OFF", "ALL", "PARTY", "GUILD", "COOP"],
+  })
+  vanquisherMode = 0;
+
+  @SwitchProperty({
+    name: "Implosion Hider",
+    description: "Hides implosion messages",
+    category: "Hiders",
+  })
+  implosionHider = false;
+
+  @SwitchProperty({
+    name: "Watchdog hider",
+    description: "Hides watchdog messages",
+    category: "Hiders",
+  })
+  watchDogHider = false;
+
+  @TextProperty({
+    name: "Watchdog message",
+    description: "Sends a message to 'thank' watchdog. Leave empty to disable",
+    placeholder: "Enter message...",
+    category: "Chat utilities",
+    subcategory: "Messages",
+  })
+  watchDogMessage = "Thanks, watchdog!";
+
+  @TextProperty({
+    name: "Kicked message",
+    description:
+      "Sends a message in party chat when you're kicked from SkyBlock. Leave empty to disable",
+    placeholder: "Enter message...",
+    category: "Chat utilities",
+    subcategory: "Messages",
+  })
+  kickedMessage =
+    "I have been kicked from SkyBlock! You can warp me back in a minute!";
+
+  @TextProperty({
+    name: "Bingo message",
+    description: "Sends a custom thank you message with /bongo",
+    placeholder: "Enter message...",
+    category: "Chat utilities",
+    subcategory: "Messages",
+  })
+  bingoMessage = "Tysm!";
 
   constructor() {
     this.initialize(this);
--- index.js
+++ index.js
@@ -3,278 +3,9 @@
 
 import Settings from "./config";
 
-// to open the config gui use the "openGUI" function
-register("command", () => Settings.openGUI()).setName("mycommand");
+register("command", () => Settings.openGUI()).setName("dt");
 
 const dripToolsPrefix = "§5§kA§a[§bDripTools§a]§5§kA§r§a ";
-
-// let vanqMode = 0;
-// let autoPVToggle = false;
-let implosionHiderToggle = false;
-let kickTimerToggle = false;
-let watchDogMessage = "Thanks, Watchdog!";
-let watchDogHider = false;
-let bingoMessage = "Tysm!";
-
-const usages = ["/dt vanq", "/dt ih", "/dt kt", "/dt wdm", "/dt wdh", "/dt bm"];
-
-register("command", (args) => {
-  if (args == null || args == "help") {
-    ChatLib.chat(dripToolsPrefix + "§7Usages: " + usages.join(", "));
-
-    return;
-  }
-
-  if (args == "vanq" || args == "vanquisher") {
-    let vanqModeState;
-    switch (Settings.vanqMode) {
-      case 0:
-        vanqModeState = "§f§lALL!";
-        Settings.vanqMode = 1;
-        break;
-      case 1:
-        vanqModeState = "§9§lPARTY!";
-        Settings.vanqMode = 2;
-        break;
-      case 2:
-        vanqModeState = "§2§lGUILD!";
-        Settings.vanqMode = 3;
-        break;
-      case 3:
-        vanqModeState = "§b§lCOOP!";
-        Settings.vanqMode = 4;
-        break;
-      case 4:
-        vanqModeState = "§c§lOFF!";
-        Settings.vanqMode = 0;
-        break;
-      default:
-        return;
-    }
-
-    ChatLib.chat(
-      dripToolsPrefix + "Vanquisher chat is set to " + vanqModeState
-    );
-
-    return;
-  }
-
-  // if (args == "pv" || args == "autopv") {
-  //   autoPVToggle = !autoPVToggle;
-
-  //   ChatLib.chat(
-  //     dripToolsPrefix +
-  //       "§aAuto PV is now " +
-  //       (autoPVToggle ? "§a§lON!" : "§c§lOFF!")
-  //   );
-  // }
-
-  if (args == "ih") {
-    implosionHiderToggle = !implosionHiderToggle;
-
-    ChatLib.chat(
-      dripToolsPrefix +
-        "Implosion hider is now " +
-        (implosionHiderToggle ? "§a§lON!" : "§c§lOFF!")
-    );
-  }
-
-  if (args == "kt" || args == "kicktimer") {
-    kickTimerToggle = !kickTimerToggle;
-
-    ChatLib.chat(
-      dripToolsPrefix +
-        "Kicked message is now " +
-        (kickTimerToggle ? "§a§lON!" : "§c§lOFF!")
-    );
-  }
-
-  if (
-    args == "wd" ||
-    args == "watchdog" ||
-    args == "wdm" ||
-    args == "watchdogmessage"
-  ) {
-    ChatLib.chat(
-      dripToolsPrefix +
-        "Current watchdog message is: " +
-        watchDogMessage +
-        "\n§aUse /wdm <message> to change it! (Max 20 words) Leave blank to disable!"
-    );
-  }
-
-  if (args == "wdh" || args == "watchdoghider") {
-    watchDogHider = !watchDogHider;
-
-    ChatLib.chat(
-      dripToolsPrefix +
-        "Watchdog hider is now " +
-        (watchDogHider ? "§a§lON!" : "§c§lOFF!")
-    );
-  }
-
-  if (args == "bm" || args == "bingomessage") {
-    ChatLib.chat(
-      dripToolsPrefix +
-        "Current bingo message is: " +
-        bingoMessage +
-        "\n§aUse /bm <message> to change it! (Max 20 words) Leave blank to disable! Use /bingo to send the message!"
-    );
-  }
-})
-  .setName("driptools")
-  .setAliases(["dt"]);
-
-register("command", () => {
-  implosionHiderToggle = !implosionHiderToggle;
-
-  ChatLib.chat(
-    dripToolsPrefix +
-      "Implosion hider is now " +
-      (implosionHiderToggle ? "§a§lON!" : "§c§lOFF!")
-  );
-}).setName("ih");
-
-register(
-  "command",
-  (
-    args,
-    args2,
-    args3,
-    args4,
-    args5,
-    args6,
-    args7,
-    args8,
-    args9,
-    args10,
-    args11,
-    args12,
-    args13,
-    args14,
-    args15,
-    args16,
-    args17,
-    args18,
-    args19,
-    args20
-  ) => {
-    watchDogMessage = "";
-    if (args == null || args == "") {
-      ChatLib.chat(dripToolsPrefix + "Watchdog message §c§lDISABLED!");
-      return;
-    }
-    var argsArray = [
-      args,
-      args2,
-      args3,
-      args4,
-      args5,
-      args6,
-      args7,
-      args8,
-      args9,
-      args10,
-      args11,
-      args12,
-      args13,
-      args14,
-      args15,
-      args16,
-      args17,
-      args18,
-      args19,
-      args20,
-    ];
-
-    for (i = 0; i < argsArray.length; i++) {
-      if (argsArray[i] == null) {
-        argsArray[i] = "";
-      }
-      watchDogMessage += argsArray[i] + " ";
-    }
-
-    ChatLib.chat(
-      dripToolsPrefix + "Watchdog message set to: " + watchDogMessage
-    );
-  }
-)
-  .setName("wdm")
-  .setAliases(["watchdogmessage"]);
-
-register(
-  "command",
-  (
-    args,
-    args2,
-    args3,
-    args4,
-    args5,
-    args6,
-    args7,
-    args8,
-    args9,
-    args10,
-    args11,
-    args12,
-    args13,
-    args14,
-    args15,
-    args16,
-    args17,
-    args18,
-    args19,
-    args20
-  ) => {
-    bingoMessage = "";
-    if (args == null || args == "") {
-      ChatLib.chat(dripToolsPrefix + "Bingo message §c§lDISABLED!");
-      return;
-    }
-    var argsArray = [
-      args,
-      args2,
-      args3,
-      args4,
-      args5,
-      args6,
-      args7,
-      args8,
-      args9,
-      args10,
-      args11,
-      args12,
-      args13,
-      args14,
-      args15,
-      args16,
-      args17,
-      args18,
-      args19,
-      args20,
-    ];
-
-    for (i = 0; i < argsArray.length; i++) {
-      if (argsArray[i] == null) {
-        argsArray[i] = "";
-      }
-      bingoMessage += argsArray[i] + " ";
-    }
-
-    ChatLib.chat(dripToolsPrefix + "Bingo message set to: " + bingoMessage);
-  }
-)
-  .setName("bm")
-  .setAliases(["bingomessage"]);
-
-register("command", () => {
-  if (bingoMessage == null || bingoMessage == "" || bingoMessage == " ") {
-    ChatLib.chat(dripToolsPrefix + "§7Bingo message is §c§lDISABLED!");
-    return;
-  }
-
-  ChatLib.command("ac " + bingoMessage);
-}).setName("bongo");
 
 register("chat", (event) => {
   if (
@@ -285,18 +16,18 @@ register("chat", (event) => {
     let messagePrefix;
     let playerLocation;
 
-    switch (Settings.vanqMode) {
+    switch (Settings.vanquisherMode) {
       case 1:
-        messagePrefix = "ac A Vanquisher has spawned at ";
+        messagePrefix = "ac";
         break;
       case 2:
-        messagePrefix = "pc A Vanquisher has spawned at ";
+        messagePrefix = "pc";
         break;
       case 3:
-        messagePrefix = "gc A Vanquisher has spawned at ";
+        messagePrefix = "gc";
         break;
       case 4:
-        messagePrefix = "cc A Vanquisher has spawned at ";
+        messagePrefix = "cc";
         break;
       default:
         return;
@@ -312,6 +43,7 @@ register("chat", (event) => {
 
     ChatLib.command(
       messagePrefix +
+        " A Vanquisher has spawned at " +
         Math.round(Player.getX()) +
         " " +
         Math.round(Player.getY()) +
@@ -340,7 +72,7 @@ register("chat", (event) => {
 // });
 
 register("chat", (event) => {
-  if (!implosionHiderToggle) {
+  if (!Settings.implosionHider) {
     return;
   }
   let message = ChatLib.getChatMessage(event, true);
@@ -353,7 +85,7 @@ register("chat", (event) => {
 });
 
 register("chat", (event) => {
-  if (!kickTimerToggle) {
+  if (Settings.kickedMessage === "") {
     return;
   }
 
@@ -364,29 +96,24 @@ register("chat", (event) => {
     message.includes("Ye be kicked while joinin' that server!")
   ) {
     ChatLib.command(
-      "pc I have been kicked from SkyBlock! You can warp me back in a minute!"
+      "pc " + kickedMessage
     );
   }
 });
 
 register("chat", (event) => {
-  if (
-    watchDogHider ||
-    watchDogMessage == null ||
-    watchDogMessage == "" ||
-    watchDogMessage == " "
-  ) {
+  if (Settings.watchDogHider) {
     return;
   }
 
   let message = ChatLib.getChatMessage(event, true);
   if (message.includes("&4[WATCHDOG ANNOUNCEMENT]&r")) {
-    ChatLib.command("ac " + watchDogMessage);
+    ChatLib.command("ac " + Settings.watchDogMessage);
   }
 });
 
 register("chat", (event) => {
-  if (!watchDogHider) {
+  if (!Settings.watchDogHider) {
     return;
   }
 
